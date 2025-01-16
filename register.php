a25d4f559a34b7b68fe340806171bfb7ff01f8a0
annisaulbilad <141201871+annisaulbilad@users.noreply.github.com>
2025-01-16 10:04:41 +0700

    register.php

--- register.php
+++ register.php
@@ -1,3 +1,6 @@
+<?php
+    include "koneksi.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Register - SB Admin</title>
+        <title>Register Ke Perpustakaan Digital</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
@@ -16,70 +19,76 @@
                 <main>
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-lg-7">
+                            <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Create Account</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Register Perpustakaan Digital</h3></div>
                                     <div class="card-body">
-                                        <form>
-                                            <div class="row mb-3">
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" />
-                                                        <label for="inputFirstName">First name</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-floating">
-                                                        <input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" />
-                                                        <label for="inputLastName">Last name</label>
-                                                    </div>
-                                                </div>
+                                        <?php
+                                             if(isset($_POST['register'])) {
+                                                 $nama = $_POST['nama'];
+                                                 $email = $_POST['email'];
+                                                 $alamat = $_POST['alamat'];
+                                                 $no_telepon = $_POST['no_telepon'];
+                                                 $username = $_POST['username'];
+                                                 $level = $_POST['level'];
+                                                 $password = md5($_POST['password']);
+
+                                                 $insert = mysqli_query($koneksi, "INSERT INTO user(nama,email,alamat,no_telepon,username,password,level) VALUES('$nama','$email','$alamat','$no_telepon','$username','$password','$level')");
+
+                                                 if($insert){
+                                                    echo '<script>alert("Selamat, register berhasil. Silahkan Login"); location.href="login.php"</script>';
+                                                 }else{
+                                                    echo '<script>alert("Register gagal, silahkan ulangi kembali.");</script>';
+                                                 }
+                                            }
+                                        ?>
+                                        <form method="post">
+                                            <div class="form-group">
+                                                <label class="small mb-1">Nama Lengkap</label>
+                                                <input class="form-control py-4" type="text" required name="nama" placeholder="Masukkan Nama Lengkap" />
                                             </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="email" placeholder="name@example.com" />
-                                                <label for="inputEmail">Email address</label>
+                                            <div class="form-group">
+                                                <label class="small mb-1">Email</label>
+                                                <input class="form-control py-4" type="text" required name="email" placeholder="Masukkan Email" />
                                             </div>
-                                            <div class="row mb-3">
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputPassword" type="password" placeholder="Create a password" />
-                                                        <label for="inputPassword">Password</label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-floating mb-3 mb-md-0">
-                                                        <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirm password" />
-                                                        <label for="inputPasswordConfirm">Confirm Password</label>
-                                                    </div>
-                                                </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1">No. Telepon</label>
+                                                <input class="form-control py-4" type="text" required name="no_telepon" placeholder="Masukkan No. Telepon" />
                                             </div>
-                                            <div class="mt-4 mb-0">
-                                                <div class="d-grid"><a class="btn btn-primary btn-block" href="login.html">Create Account</a></div>
+                                            <div class="form-group">
+                                                <label class="small mb-1">Alamat</label>
+                                                <textarea name="alamat" rows="5" required class="form-control"></textarea>
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1">Username</label>
+                                                <input class="form-control py-4" type="username" required name="username" placeholder="Masukkan username" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress">Password</label>
+                                                <input class="form-control py-4" id="inputPassword" type="password" required name="password" placeholder="Masukkan password" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1">Level</label>
+                                                <select name="level" required class="form-control">
+                                                    <option value="peminjam">Peminjam</option>
+                                                    <option value="admin">Admin</option>
+                                                </select>       
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <button class="btn btn-primary"  type="submit" name="register" value="register">Register</button>
                                             </div>
                                         </form>
                                     </div>
-                                    <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="login.html">Have an account? Go to login</a></div>
+                                    <div class="card-footer text-center">
+                                        <div class="small">
+                                            &copy; 2025 perpustakaan Digital
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
